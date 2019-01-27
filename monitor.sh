66ef59cf457f3e98f962de7eb5ffa0dc615389b6
mohamed larit <25457545+mohamed1lar@users.noreply.github.com>
2019-01-27 14:07:10 +0100

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -2,8 +2,7 @@
 
 mem=$(free -m | awk 'NR==2{print $3*100/$2}' | awk -F. '{print $1}')
 disk=$(df -h | awk '$NF=="/"{print $5}' | awk -F% '{print $1}')
-cpu=$(top -bn1 | grep load | awk '{print $(NF-2)}' | awk -F. '{print $1}')
-
+cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage ""}' | awk -F. '{print $1}')
 
 if [ "$cpu" -ge "80" ]
 then
