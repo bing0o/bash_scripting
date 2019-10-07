5092b72bc03d7d3e013571e204e92f6a98bafb7c
med pro <25457545+bing0o@users.noreply.github.com>
2019-10-07 17:21:42 +0100

    Update autorecon.sh

--- autorecon.sh
+++ autorecon.sh
@@ -7,16 +7,19 @@ purple="\e[35m"
 
 read -p "[+] Enter Domain: " domain
 
-# link: https://github.com/aboul3la/Sublist3r
-echo -e $cyan"[*] Start Sublist3r"$end
-sublist3r -d $domain -o "$domain-list" &>/dev/null
+# link: https://github.com/Edu4rdSHL/findomain
+echo -e $cyan"[*] Start Findomain"$end
+findomain -t $domain -o &>/dev/null
+echo "	* Results saved in: $domain.txt"
 
 # link: https://github.com/bing0o/Python-Scripts/blob/master/subchecker.py
 echo -e $cyan"[*] Start Subchecker"$end
-subchecker -w "$domain-list" -t 30 -o "$domain-checked" &>/dev/null
+subchecker -w "$domain.txt" -t 30 -o "$domain-checked" &>/dev/null
+echo "	* Results saved in: $domain-checked"
 
 # link: https://github.com/bing0o/Python-Scripts/blob/master/webtech.py
 echo -e $cyan"[*] Start WebTech"$end
 webtech -w "$domain-checked" -t 30 -o "$domain-Tech" -i &>/dev/null
+echo "	* Results saved in: $domain-Tech"
 
 echo -e $purple"[!] You Did Nothing Yet,Go Deeper!"$end
