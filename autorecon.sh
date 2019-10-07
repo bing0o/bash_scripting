13d54131150a6f2312b0e7b2b2fb61a1fc527234
med pro <25457545+bing0o@users.noreply.github.com>
2019-10-07 17:55:28 +0100

    Update autorecon.sh

--- autorecon.sh
+++ autorecon.sh
@@ -8,21 +8,25 @@ purple="\e[35m"
 read -p "[+] Enter Domain: " domain
 
 # link: https://github.com/Edu4rdSHL/findomain
+echo ""
 echo -e $cyan"[*] Start Findomain"$end # Getting a list of subdomains
 findomain -t $domain -o &>/dev/null
 size=$(wc -l $domain.txt)
 echo "[+] Results: $size"
 
 # link: https://github.com/bing0o/Python-Scripts/blob/master/subchecker.py
+echo ""
 echo -e $cyan"[*] Start Subchecker"$end # Filter the result and save only the live subdomains
 subchecker -w "$domain.txt" -t 30 -o "$domain-checked" &>/dev/null
 size=$(wc -l $domain-checked)
 echo "[+] Results: $size"
 
 # link: https://github.com/bing0o/Python-Scripts/blob/master/webtech.py
+echo ""
 echo -e $cyan"[*] Start WebTech"$end # Getting the technologies that running in each subdomain
 webtech -w "$domain-checked" -t 30 -o "$domain-Tech" -i &>/dev/null
 size=$(wc -l $domain-Tech)
 echo "[+] Results: $size"
 
+echo ""
 echo -e $purple"[!] You Did Nothing Yet,Go Deeper!"$end
