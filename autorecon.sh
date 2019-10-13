ff881ed367006a6a7378b9efaff09a691c3fd849
med pro <25457545+bing0o@users.noreply.github.com>
2019-10-13 14:05:24 +0100

    Update autorecon.sh

--- autorecon.sh
+++ autorecon.sh
@@ -7,6 +7,12 @@ purple="\e[35m"
 
 read -p "[+] Enter Domain: " domain
 
+# https://github.com/s0md3v/Photon
+echo ""
+echo -e $cyan"[*] Start Photon"$end # mapping the target domain
+photon "-u $domain -t 30 --wayback --dns --keys" 1>/dev/null
+echo "[+] Results: In $domain Folder"
+
 # link: https://github.com/Edu4rdSHL/findomain
 echo ""
 echo -e $cyan"[*] Start Findomain"$end # Getting a list of subdomains
@@ -17,14 +23,14 @@ echo "[+] Results: $size"
 # link: https://github.com/bing0o/Python-Scripts/blob/master/subchecker.py
 echo ""
 echo -e $cyan"[*] Start Subchecker"$end # Filter the result and save only the live subdomains
-subchecker -w "$domain.txt" -t 30 -o "$domain-checked" &>/dev/null
+subchecker -w "$domain.txt" -t 30 -o "$domain-checked" 1>/dev/null
 size=$(wc -l $domain-checked)
 echo "[+] Results: $size"
 
 # link: https://github.com/bing0o/Python-Scripts/blob/master/webtech.py
 echo ""
 echo -e $cyan"[*] Start WebTech"$end # Getting the technologies that running in each subdomain
-webtech -w "$domain-checked" -t 30 -o "$domain-Tech" -i &>/dev/null
+webtech -w "$domain-checked" -t 30 -o "$domain-Tech" -i 1>/dev/null
 size=$(wc -l $domain-Tech)
 echo "[+] Results: $size"
 
