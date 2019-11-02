00915a3552b3a36848dffd20a0788dd5979aceb3
med pro <25457545+bing0o@users.noreply.github.com>
2019-11-02 21:41:37 +0100

    Add files via upload

---
+++ awscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# cat ips | xargs -I% sh -c "host %"
+# -z True if it's a zero | -n True if it's not a zero!
+[ -z $1 ] && echo "#Usage: awscript.sh <domains/ips>" && exit 
+
+lines=$(wc -l $1 | awk '{print $1}')
+c=1
+while read line; do 
+	echo -ne "[$c/$lines] $line                                                 \r"
+	let c=c+1
+	res=$(host $line)
+	if [[ $res == *"amazonaws"* ]]; then 
+		echo $line" | " $res
+	fi
+done < $1 
