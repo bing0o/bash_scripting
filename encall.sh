417219dec2899a122c8a02b0556c0f2018625b05
med pro <25457545+bing0o@users.noreply.github.com>
2019-11-22 20:02:48 +0100

    Update encall.sh

--- encall.sh
+++ encall.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-# script to encrypt all the files in a specific directory | https://github.com/bing0o/Python-Scripts/blob/master/crypto.py
+
 if [ -z $1 ] 
 then 
-	echo "#Usage: encall <OPTIONS (e/d)>"
+	echo "#Usage: encall <OPTIONS (e/d)> <PASSWORD, default (P4ssw@rD)>"
 	exit 1
 fi
 
-# -f for files & -d for directories
+[[ -z $2 ]] && password="P4ssw@rD" || password=$2
+
 if [ "$1" == "e" ]
 then
-	ls * | xargs -I% sh -c "echo '[+] Processing: %' && if [ -f % ]; then crypto -e % -p password -x 1>/dev/null; fi"
+	ls * | grep -v ".hacklab$" | xargs -I% bash -c "echo -ne \"[+] Processing: %                                             \r\" && if [ -f % ]; then crypto -e % -p $password -x 1>/dev/null; fi"
 fi
 
 if [ "$1" == "d" ]
 then
-	ls *.hacklab | xargs -I% sh -c "echo '[+] Processing: %' ; crypto -d % -p password -x 1>/dev/null"
+	ls *.hacklab | xargs -I% bash -c "echo -ne \"[+] Processing: %                                                    \r\" ;crypto -d % -p $password -x 1>/dev/null"
 fi
-	
+echo -ne "[*] Done!                                        \r"
+echo ""
