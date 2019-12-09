f5c27591610a86525b090c64abacc59ea5b614af
terminalforlife <terminalforlife@yahoo.com>
2019-12-09 22:59:23 +0000

    Revise and overhaul `encall.sh`
    I checked out to e41f7317a9ad8d5f27095a6908d7cf888 commit, made by
    @bing0o on November the 23rd, then just redid the file. I went all out
    this time. Hopefully I didn't overlook something.
    
    Bug Fixes:
    
      * Should now address the _All-Elusive Argument of 2019_ issue.
    
      * Caught error when the user enters no password but uses its flag.
    
      * No longer parses `ls`, thus avoiding lots of unwanted effects.
    
    Additions:
    
      * Reworked the header, and fancied it up a bit.
    
      * Added `CurVer` and `Progrm` variables.
    
      * Added `Err()` for consistent and presentable error messages.
    
      * Added an informative `Usage()` function, akin to my own programs.
    
      * Added each of us (your E-Mail pending, if applicable) to `Usage()`.
    
      * Reworked the description, especially to be in line with changes.
    
      * The synopsis or syntax is now more traditional, and easier to read.
    
      * Preferred the standard GNU long and short flag styling: `--`, `-`
    
      * Added long options for the old arguments.
    
      * Added `--help|-h|-?` and `--version|-v` options.
    
      * Added a `SITE:` link to both of your related repositories.
    
      * Now iterates over whichever files are specified by the user.
    
      * Tweaked the `Processing:` message to be more situational.
    
    Code:
    
      * Prefer `printf` over `echo`, for portability and functionality.
    
      * Written and converted to POSIX Bourne for excellent portability.

--- encall.sh
+++ encall.sh
@@ -1,29 +1,95 @@
-#!/bin/bash
+#!/bin/sh
+#cito M:755 O:0 G:0 T:/usr/local/bin/crypto
+#----------------------------------------------------------------------------------
+# A Bourne POSIX shell script acting as a wrapper for another written in Python, -
+# called 'crypto.py', available with the following:
 #
-# script to encrypt all the files in a specific directory, using crypto.py python script.
+#   wget https://raw.githubusercontent.com/bing0o/Python-Scripts/master/crypto.py
+#   chmod 755 crypto.py; chown 0:0 crypto.py
+#   sudo mv crypto.py /usr/local/bin/crypto
 #
-# wget https://raw.githubusercontent.com/bing0o/Python-Scripts/master/crypto.py
-# chmod +x crypto.py
-# sudo cp crypto.py /usr/local/bin/crypto
+# If you have Cito (https://github.com/terminalforlife/Extra):
 #
-# change the default password!.
+#   sudo cito -r bing0o Python-Scripts master crypto.py
+#
+# WARNING: Change the default password!
+#----------------------------------------------------------------------------------
 
-Do()
-{
-	setterm -linewrap off
-	find -maxdepth 1 \( -type f $1 -name '*.hacklab'\
-		-exec crypto $1 {} -p ${2:-P4ssw@rD} -x 1>/dev/null \;\
-		-exec tput el \; \) -printf "%p\r"
-	setterm -linewrap on
-	echo "[*] Done!"
+CurVer='2019-12-09'
+Progrm=${0##*/}
+
+Err(){
+	printf "ERROR: %s\n" "$2" 1>&2
+	[ $1 -gt 0 ] && exit $1
 }
 
-case $1 in
-	e)
-		Do -not -e ;;
-	d)
-		Do ' ' -d ;;
-	''|*)
-		echo "Usage: encall <OPTIONS (e/d)> <PASSWORD, default (P4ssw@rD)>"
-		exit 1 ;;
-esac
+Domain='https://github.com'
+
+Usage(){
+	while read -r CurLine; do
+		printf "%b\n" "$CurLine"
+	done <<-EOF
+		\r            ENCALL ($CurVer)
+		\r            Originally written by bing0o <your E-Mail here>
+		\r            Revised by terminalforlife <terminalforlife@yahoo.com>
+
+		\r            A simple Bourne POSIX wrapper for Python script Crypto.
+
+		\rSYNTAX:     $Progrm [OPTS] [FILE_1 [FILE_2] ...]
+
+		\rOPTS:       --help|-h|-?            - Displays this help information.
+		\r            --version|-v            - Output only the version datestamp.
+		\r            --encrypt|-e            - Encrypts one or more files.
+		\r            --decrypt|-d            - Decrypts one or more files.
+		\r            --password|-p STR       - Where STR is the password to use.
+
+		\rSITE:       $Domain/bing0o/bash_scripting
+		\r            $Domain/bing0o/Python-Scripts
+	EOF
+}
+
+Password='P4ssw@rD'
+
+while [ "$1" ]; do
+	case $1 in
+		--help|-h|-\?)
+			Usage; exit 0 ;;
+		--version|-v)
+			printf "%s\n" "$CurVer"; exit 0 ;;
+		--encrypt|-e)
+			Action='-e'
+			Actioning='Encrypting' ;;
+		--decrypt|-d)
+			Action='-d'
+			Actioning='Decrypting' ;;
+		--password|-p)
+			shift
+
+			if [ -z "$1" ]; then
+				Err 1 "Password mising for the '--password|-p' option."
+			else
+				Password=$1
+			fi ;;
+		-*)
+			Err 1 "Invalid argument(s) detected -- see: '$'" ;;
+		*)
+			break ;;
+	esac
+	shift
+done
+
+for CurFile in ./*; do
+	[ -f "$CurFile" ] || continue
+
+	# Ignore `*.hacklab` files if encrypting.
+	if [ "$Action" = '-e' ]; then
+		case $CurFile in
+			*.hacklab) continue ;;
+		esac
+	fi
+
+	printf "[+] %s: %s\n" "$Actioning" "$CurFile"
+	crypto "$Action" "$CurFile" -p "$Password" -x 1> /dev/null
+done
+
+printf "[*] Done!\n"
