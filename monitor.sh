b86b8716d7a71d5934ece99c93bad0bb0e88cafa
terminalforlife <terminalforlife@yahoo.com>
2019-12-14 12:26:24 +0000

    Optimize `monitor.sh` (fewer processes)
    The process by which this script fetches the RAM usage is now done only
    with `free` and the Bourne shell, so no more AWK needed.
    
    It's a shame pipes are needed, since a subshell is created for each one
    used, although it looks like that behavior is shell-dependent.
    
      "The POSIX standard allows executing any of subcommands in the
      current shell, rather than subshells, ..." - yash(1)
    
    Using pipes can wind up to be a lot of new processes. In Bash, though -
    using `<<< ""` (herestring) is done within the current shell. This is
    part of the reason why, in Bash, I favor this sort of while read loop:
    
      ```bash
      while read CurLine; do
        printf '%s\n' "$CurLine"
      done <<< "$Data"
      ```
    
    Plus I find it easier to read, since the while part is the first word.
    
    Hope that was interesting and educational, albeit entirely unnecessary
    for a commit message.

--- monitor.sh
+++ monitor.sh
@@ -1,31 +1,27 @@
 #!/bin/sh
 
-DepCount=0
-for CurDep in awk free notify-send df; do
-	if ! command -v "$CurDep" 1> /dev/null 2>&1; then
-		printf "ERROR: Dependency '%s' not met.\n" "$CurDep"
-		DepCount=$((DepCount + 1))
+cpu=$(awk '/^cpu/ {usage=($2+$4)*100/($2+$4+$5)} END{printf("%d\n", usage)}' /proc/stat)
+[ $cpu -ge 80 ] && notify-send -u critical "High CPU usage: ${cpu}%"
+
+free -m | while read Type Total Used Free _; do
+	if [ "$Type" = 'Mem:' ]; then
+		PCent=$((Used*100/Total))
+
+		[ $PCent -ge 80 ] && notify-send -u critical "High RAM usage: ${PCent}%"
 	fi
 done
-
-[ $DepCount -eq 0 ] || exit 1
-
-cpu=$(awk '/^cpu/ {usage=($2+$4)*100/($2+$4+$5)} END{printf("%d\n", usage)}' /proc/stat)
-[ $cpu -ge 80 ] && notify-send -u critical "The CPU Almost Filled: $cpu"
-
-mem=$(free -m | awk 'NR==2{printf("%d\n", $3*100/$2)}')
-[ $mem -ge 80 ] && notify-send -u critical "The MEMORY Almost Filled: $mem"
 
 # Bourne POSIX-compliant approach.
 df -P / | while read F1 _ _ _ F5 _; do
 	if ! [ "$F1" = 'Filesystem' ]; then
-		Percent=${F5%\%}
-		if [ $Percent -ge 80 ]; then
-			notify-send -u critical "The DISK Almost Filled: $Percent"
-		fi
+		PCent=${F5%\%}
+
+		[ $PCent -ge 80 ] && notify-send -u critical "High DISK usage: ${PCent}%"
 
 		# In-case there are for some reason >1 matches.
 		break
 	fi
 done
 
+# In-case we're running in a cron job.
+exit 0
