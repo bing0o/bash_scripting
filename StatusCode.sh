4059ac41b59e5bce8210323d4ba38be1f686f4dc
med pro <25457545+bing0o@users.noreply.github.com>
2020-03-11 18:14:34 +0100

    Update StatusCode.sh

--- StatusCode.sh
+++ StatusCode.sh
@@ -3,5 +3,4 @@
 [ -z $1 ] && { echo -e "#Usage:\n\tcodesh.sh <DOMAINS> <THREADS:5>"; exit 1; }
 [ -z $2 ] && t=5 || t=$2
 
-cat $1 | parallel -j$t -q curl --connect-timeout 10 -w '%{http_code},%{size_download},%{url_effective},%{redirect_url}\n' -o /dev/null -sk
-
+cat $1 | xargs -P $t -I{} bash -c "curl {} --connect-timeout 10 -w '%{http_code},%{url_effective},%{size_download},%{redirect_url}\n' -o /dev/null -sk"
