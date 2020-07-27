b8b44bc716b42a3bd7019611b5eb2c76f54ddf1c
med pro <25457545+bing0o@users.noreply.github.com>
2020-07-27 20:43:17 +0100

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -13,6 +13,6 @@
 
 temps="$HOME/tools/nuclei-templates/all/"
 
-subenum -s -d $1 | sort -u | tee subs-$1 | filter-resolved -c 50 | sudo $(which naabu) -silent -t 50 -ports 80,81,443,3000,6443,8000,8001,8008,8080,8083,8443,8834,8888,9090 | httprobe -c 50 | tee hosts-$1 | sort -u | nuclei -c 50 -t "$temps" -o nuclei-$1.logs
+subenum -s -d $1 | tee subs-$1 | filter-resolved -c 50 | sudo $(which naabu) -silent -t 50 -ports 80,81,443,3000,6443,8000,8001,8008,8080,8083,8443,8834,8888,9090 | httprobe -c 50 | tee hosts-$1 | sort -u | nuclei -c 50 -t "$temps" -o nuclei-$1.logs
 
 
