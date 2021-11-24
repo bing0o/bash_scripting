857194eb7a00fc49582a949d0cd344f157dedbd9
med pro <25457545+bing0o@users.noreply.github.com>
2021-11-24 13:23:20 +0100

    Update naabu-to-nmap.sh

--- naabu-to-nmap.sh
+++ naabu-to-nmap.sh
@@ -9,15 +9,22 @@
 
 list=$(cat "$1" | cut -d ':' -f 1 | sort -u)
 
-count=1
-#length=${#list[@]}
+all=()
 
+for i in ${list[@]}
+do
+        all+=("$i")
+done
+
+length=${#all[@]}
+
+count=1
 for IP in ${list[@]}
 do
-        printf "[+] Scanning ($count)"
+        printf "[+] Scanning ($count/$length)"
         printf "                                        \r"
         ports=$(cat "$1" | grep "$IP" | cut -d ":" -f 2)
         ports=$(echo $ports | tr ' ' ',' ) #awk '{gsub(/\n/,",,",$0); print $0}')
-        nmap -sV -T4 "$IP" -p "$ports" -oN "nmap-results/$IP" &>/dev/null
+        nmap --script default -sV -T5 "$IP" -p "$ports" -oN "nmap-results/$IP" --open -Pn &>/dev/null
         let count+=1
 done
