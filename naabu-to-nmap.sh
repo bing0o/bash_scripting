0e06b26bba5bce6c378375b02a87b986eb62cce8
med pro <25457545+bing0o@users.noreply.github.com>
2022-04-05 02:11:09 +0100

    Exclude port 8080 and 8443 if there's too much

--- naabu-to-nmap.sh
+++ naabu-to-nmap.sh
@@ -7,13 +7,25 @@
 
 [ -d "nmap-results" ] || mkdir "nmap-results"
 
+
+E8080=False # Exclude port 8080 from the scan
+E8443=False # Exclude port 8443 from the scan
+
+C8080=$(cat $1 | grep ':8080$' | wc -l) # Count Port 8080
+C8443=$(cat $1 | grep ':8443$' | wc -l) # Count Port 8443
+
+Exclude=()
+
+[[ ${C8080} -ge 20 ]] && Exclude+=("8080") #E8080=True
+[[ ${C8443} -ge 20 ]] && Exclude+=("8443") #E8443=True
+
 list=$(cat "$1" | cut -d ':' -f 1 | sort -u)
 
 all=()
 
 for i in ${list[@]}
 do
-        all+=("$i")
+	all+=("$i")
 done
 
 length=${#all[@]}
@@ -21,10 +33,16 @@ length=${#all[@]}
 count=1
 for IP in ${list[@]}
 do
-        printf "[+] Scanning ($count/$length)"
-        printf "                                        \r"
-        ports=$(cat "$1" | grep "$IP" | cut -d ":" -f 2)
-        ports=$(echo $ports | tr ' ' ',' ) #awk '{gsub(/\n/,",,",$0); print $0}')
-        nmap --script default -sV -T5 "$IP" -p "$ports" -oN "nmap-results/$IP" --open -Pn &>/dev/null
+        printf "[+] [$count/$length] Scanning: ${IP} "
+        printf "                                                     \r"
+        ports=$(cat "$1" | sort -u | grep "^${IP}" | cut -d ":" -f 2)
+
+	for i in ${Exclude[@]}
+	do
+		ports=${ports[@]/$i} # Delete The Excluded Port From The List of Ports
+	done
+
+        ports=$(echo $ports | tr ' ' ',' )
+        nmap --script default,vuln -sV -T4 "$IP" -p "$ports" -oN "nmap-results/$IP" --open -Pn &>/dev/null # Run nmap scan with the (default and vuln) scripts.
         let count+=1
 done
