e49c500c591e27d9e91b90578b90f6360f665238
med pro <25457545+bing0o@users.noreply.github.com>
2023-07-25 14:14:09 +0100

    Update vpn_on_vps.sh

--- vpn_on_vps.sh
+++ vpn_on_vps.sh
@@ -6,6 +6,11 @@
 # but not sure about DNS leaks and other stuff that could expose your IP,
 # be carefull when using this method.
 #
+# 1. SSH to your VPS.
+# 2. Run this script.
+# 3. Run your VPN client.
+#
+# your connection will not be closed and the ip of your VPS will be changed.
 
 
 myip=$(curl ifconfig.me -sk)
@@ -17,4 +22,4 @@ baseip=$(echo ${myip} | cut -d"." -f1-3)
 ip route add table 137 to ${baseip}.0/24 dev eth0
 ip route add table 137 default via ${baseip}.1
 
-printf "[+] Now run your Openvpn in a tmux or screen session.!"
+printf "[+] Now run your Openvpn in a tmux or screen session.!\n"
